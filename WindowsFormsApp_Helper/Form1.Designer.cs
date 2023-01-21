53b290a821559b7ec0a4607d99f58e769279ccbc
yutaro-takada <70902396+yutaro-takada@users.noreply.github.com>
2023-01-21 21:59:55 +0900

    ChangeTextメソッド実装

--- WindowsFormsApp_Helper/Form1.Designer.cs
+++ WindowsFormsApp_Helper/Form1.Designer.cs
@@ -29,32 +29,68 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 29);
+            this.button1.Location = new System.Drawing.Point(144, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 66);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 156);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp_Helper/Form1.cs
+++ WindowsFormsApp_Helper/Form1.cs
@@ -16,5 +16,55 @@ namespace WindowsFormsApp_Helper
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string[] textList = textBox1.Text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+                string addText = ChangeText(textList);
+                MessageBox.Show(addText, "コピー完了");
+                Clipboard.SetText(addText);
+                this.textBox1.Clear();
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                string[] textList = textBox2.Text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+                string addText = ChangeText(textList);
+                MessageBox.Show(addText, "コピー完了");
+                Clipboard.SetText(addText);
+                this.textBox2.Clear();
+            }
+        }
+        private string ChangeText(string[] text)
+        {
+            string[] array = new string[text.Length];
+            if (text.Length == 1)
+            {
+                array[0] = string.Format("'{0}'", text[0]);
+            }
+            else
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (i == text.Length - 1)
+                    {
+                        array[i] = string.Format("'{0}'", text[i]).ToString();
+                    }
+                    else
+                    {
+                        array[i] = string.Format("'{0}',", text[i]).ToString();
+                    }
+                }
+            }
+            return string.Join("\r\n", array);
+        }
     }
 }
